94b77cf89a3819d5bf0a5b3b3bc26742de9fbd40
Russell Hay <me@russellhay.com>
2016-08-24 07:10:28 -0700

    finishing up the very basic profile, disabling dropping or updating

--- server/lib/profile/index.js
+++ server/lib/profile/index.js
@@ -2,24 +2,45 @@ var restify = require('restify');
 var auth = require('../auth');
 var Model = require('./model');
 
+function getAllProfiles (req, res, next) {
+  var model = Model(req.db);
+  model.all().then((userList) => {
+    res.send({code: 200, data: userList});
+  }).catch((err) => {
+    next(err);
+  });
+}
+
 function getProfile (req, res, next) {
   var includeOwnInformation = (req.user && req.user.username == req.params.username);
   var model = Model(req.db);
-  model.find(req.params.username, (err, user) => {
+  model.find(req.params.username).then((user) => {
     if (!user) {
       return next(new restify.errors.ResourceNotFoundError(`/users/${req.params.username} not found`));
     }
     delete user['password_hash']; // TODO: just remove it from the query based on the includeOwnInformation
     res.send({code: 200, data: user});
+  }).catch((err) => {
+    next(err);
   });
 }
 
 function updateProfile (req, res, next) {
-  res.send('update');
+  var model =  new Model(req.db);
+  model.update(req.user.id, JSON.parse(req.body)).then((value) => {
+    res.send({code: 200, data: value});
+  }).catch((err) => {
+    next(err);
+  })
 }
 
 function createProfile (req, res, next) {
-  res.send('create');
+  var model = new Model(req.db);
+  model.create(JSON.parse(req.body)).then((user) => {
+    res.send({code: 200, data: user});
+  }).catch((err) => {
+    next(err);
+  });
 }
 
 function deleteProfile (req, res, next) {
@@ -29,25 +50,30 @@ function deleteProfile (req, res, next) {
 
   var model = Model(req.db);
 
-  model.find(req.params.username, (err, user) => {
-    next.ifError(err);
+  model.find(req.params.username).then((user) => {
+    req.log.info('find done');
     if (!user) {
       next(new restify.errors.ResourceNotFoundError(`${req.params.username} not found`));
     }
-    model.drop(user.id, (err) => {
-      next.ifError(err);
+    req.log.info('user', user);
+    return model.drop(user.id);
+  }).then((value) => {
+      req.log.info('drop done', err);
       res.send({code: 200});
-    });
-  });
+  }).catch((err) => {
+    res.log.err('DROP ERROR', err);
+    next.ifError(err);
+  })
 }
 
 module.exports = (server) => {
 
   // Register Routes for profile
   server.get('/user/:username', getProfile);
+  server.get('/users', getAllProfiles);
 
-  server.post('/user/:username', auth.authenticate, auth.is_owner, updateProfile);
-  server.del('/user/:username', auth.authenticate, auth.is_admin, deleteProfile);
+  // server.post('/user/:username', auth.authenticate, auth.is_owner, updateProfile);
+  // server.del('/user/:username', auth.authenticate, auth.is_admin, deleteProfile);
   server.put('/user', auth.authenticate, auth.is_admin, createProfile);
 
 };
--- server/lib/profile/model.js
+++ server/lib/profile/model.js
@@ -1,4 +1,12 @@
 var log = require('bunyan').createLogger({name: 'profile'});
+var hash = require('../hasher');
+
+function _pass_onto_promise(resolve, reject) {
+  return (err, value) => {
+    if (err) { reject(err); }
+    resolve(value);
+  }
+}
 
 function model(db) {
   if (!(this instanceof model)) {
@@ -8,24 +16,56 @@ function model(db) {
   this.db = db;
 }
 
-model.prototype.find = function (username, cb) {
-  this.db.users.findOne({username: username}, cb);
+model.prototype.all = function () {
+  return new Promise((resolve, reject) => {
+    this.db.users_all(_pass_onto_promise(resolve, reject));
+  });
+}
+
+model.prototype.find = function (username) {
+  return new Promise((resolve, reject) => {
+    this.db.users.findOne({username: username}, _pass_onto_promise(resolve, reject))
+  });
 };
 
-model.prototype.get = function (id, cb) {
-  this.db.users.find(id, cb);
-};
-
-model.prototype.drop = function (id, cb) {
-  this.db.users.destroy({id: id}, cb);
-};
-
-model.prototype.create = function (data, cb) {
+model.prototype.get = function (id) {
+  return new Promise((resolve, reject) => {
+    this.db.users.find(id, _pass_onto_promise(resolve, reject));
+  });
 
 };
 
-model.prototype.update = function (data, cb) {
+// model.prototype.drop = function (id) {
+//   log.info('drop called');
+//   return new Promise((resolve, reject) => {
+//     log.info('drop promise started');
+//     this.db.users.destroy({id: id}, _pass_onto_promise(resolve, reject));
+//   });
+// };
 
+model.prototype.create = function (data) {
+  log.info(`creating user ${data.username}`);
+  var password = data.password;
+  delete data['password'];
+  data.password_hash = hash.getHash(password);
+  return new Promise((resolve, reject) => {
+    this.db.users.save(data, _pass_onto_promise(resolve, reject));
+  });
 };
+
+// model.prototype.update = function (id, data) {
+//   var updateRecord = {
+//     id: id
+//   };
+//   if (data.username) {
+//     updateRecord.username = data.username
+//   }
+//   if (data.password) {
+//     // updateRecord.password_hash = ""; // TODO: generate password hash
+//   }
+//   return new Promise((resolve, reject) => {
+//     this.db.users.save(updateRecord, _pass_onto_promise(resolve, reject));
+//   });
+// };
 
 module.exports = model;
